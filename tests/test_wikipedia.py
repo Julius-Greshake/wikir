c777fd0edad70cfaeb281634af78b28108016e62
Julius Greshake <juliusgreshake@gmail.com>
2021-10-25 23:00:24 +0200

    Initial commit - end of Hypermodern Ch. 3

---
+++ tests/test_wikipedia.py
@@ -0,0 +1,7 @@
+from wikir import wikipedia
+
+
+def test_random_page_usses_given_language(mock_requests_get):
+    wikipedia.random_page(language="de")
+    args, _ = mock_requests_get.call_args
+    assert "de.wikipedia.org" in args[0]
