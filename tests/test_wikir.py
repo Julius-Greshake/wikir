bb7156318256ff3d765f81c74f65308b26b5afb2
Julius Greshake <juliusgreshake@gmail.com>
2021-10-26 22:06:27 +0200

    Add docstrings to test suite

--- tests/test_wikir.py
+++ tests/test_wikir.py
@@ -1,5 +1,7 @@
+"""Test cases for the wikir package."""
 from wikir import __version__
 
 
 def test_version() -> None:
+    """It returns the correct version number."""
     assert __version__ == "0.1.0"
