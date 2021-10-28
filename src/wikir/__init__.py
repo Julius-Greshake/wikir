086d24ff90d3f2232e39eade0c3bc01b6afb990c
Julius Greshake <juliusgreshake@gmail.com>
2021-10-28 12:15:11 +0200

    Make pyproject.toml single source of truth for package version

--- src/wikir/__init__.py
+++ src/wikir/__init__.py
@@ -1,2 +1,11 @@
 """Print a random wikipedia page to the console."""
-__version__ = "0.1.0"
+try:
+    from importlib.metadat import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pgrama: no cov er
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
